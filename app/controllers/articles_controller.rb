3abf3580ba444c78b5f89adf41c88d49fbcac99d
bhawan0407 <bhawandtu93@gmail.com>
2020-07-10 09:34:49 +0530

    Added index and new actions in articles controller and the coresponding views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,23 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    #render plain: params[:article]
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+    # redirect_to @article # same as above
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
