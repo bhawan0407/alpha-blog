99c79cbb4c54befe20e0349754712ca6fccab7cb
bhawan0407 <bhawandtu93@gmail.com>
2020-07-11 20:55:07 +0530

    Added integration tests for admin and categories

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -3,6 +3,13 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @admin_user = User.create(username: "johndoe", email: "johndoe@example.com",
+                              password: "password", admin: true)
+    sign_in_as(@admin_user)
+  end
+
   test "get new category form and create category" do
     get "/categories/new"
     assert_response :success
@@ -25,4 +32,5 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert'
     assert_select 'h4.alert-heading'
   end
+
 end
